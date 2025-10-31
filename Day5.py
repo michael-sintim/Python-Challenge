302f6c33dab4634877545bd516d138df7fa1739f
Michael Sintim <michaelsintim673@gmail.com>
2025-10-31 18:12:52 +0000

    Day 5 of my journey

---
+++ Day5.py
@@ -0,0 +1,9 @@
+#converts celsius to fareheit
+
+def celsius_to_fahrenheit(celsius):
+    # Your code here
+    Fahrenheit = (celsius* 1.8) + 32
+    return Fahrenheit
+
+print(celsius_to_fahrenheit(0))  
+print(celsius_to_fahrenheit(100))
