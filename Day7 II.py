6fcd31ea6c3d152ef1ff041cadb4c0de2fef4ed8
Michael Sintim <michaelsintim673@gmail.com>
2025-11-02 16:36:19 +0000

    Python programming practice

--- Day7 II.py
+++ Day7 II.py
@@ -1,43 +1,61 @@
 from Day7 import password_validator
 
-
-if password_validator() == "Password accepted":
-    print("\n===SECURE CALCULATOR===")
-    print("1. Perform calculation")
-    print("2. View history")
-    print("3. Clear history")
-    print("4. Exit\n")
-
-choice = input("Input choice: ")
-
-def calculate(x: float ,y: float,operand: str)-> float:
-    x = float(input("Input a number:"))
-    y = float(input("Input a number:"))
-    operand = input("Input a operand:")
-
-    try:
-        
-        if x/0 or y/0:
-            raise ZeroDivisionError
-        
-        elif operand == "+":
-            return x + y
-
-        elif operand == "-":
-
-            return x -y
-        elif operand == "*":
-
-            return x * y
-        elif operand == "/":
-            return x / y
-        elif operand == "**":
-            return x ** y
-        elif operand == "%":
-            return x % y
-        
-    except:
-        raise ValueError("Invalid operand")
+while True: 
     
-# if __name__ == "__main__":
-    calculate()
+    def calculate(x: float ,y: float,operand: str)-> float:
+        x = float(input("Input a number:"))
+        y = float(input("Input a number:"))
+        operand = input("Input a operand:")
+
+        try:
+
+            if x/0 or y/0:
+                raise ZeroDivisionError
+
+            elif operand == "+":
+                return x + y
+
+            elif operand == "-":
+
+
+                return x -y
+            elif operand == "*":
+
+                return x * y
+            elif operand == "/":
+                return x / y
+            elif operand == "**":
+                return x ** y
+            elif operand == "%":
+                return x % y
+
+        except:
+            raise ValueError("Invalid operand")
+    
+    if password_validator() == "Password accepted":
+
+        history = []
+
+        print("\n===SECURE CALCULATOR===")
+        print("1. Perform calculation")
+        print("2. View history")
+        print("3. Clear history")
+        print("4. Exit\n")
+    
+    choice = input("Input choice: ")
+
+
+    if choice == '1':
+        calculate()
+
+
+    elif choice == '2':
+        pass
+    
+    elif choice == '3':
+        pass
+
+    elif choice == '4':
+        print("Your program is over")
+        quit()
+    
