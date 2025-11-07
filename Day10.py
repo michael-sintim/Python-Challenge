a8a31d76771ca159e6677bcd5a1ec3e67accb68d
Michael Sintim <michaelsintim673@gmail.com>
2025-11-07 09:58:52 +0000

    Python Programming

--- Day10.py
+++ Day10.py
@@ -1,2 +1,13 @@
 #Day 10
 ## Build a program that converts between JSON and CSV formats with  serialization
+
+name = input("Input name: ")
+age = input("Input age: ")
+email = input("Input email: ")
+
+contact = {
+    "Name":name,
+    "Age" : age,
+    "Email": email,
+}
+
