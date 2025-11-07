def68d880b691319f5861a4701a9f02eb5935df8
Michael Sintim <michaelsintim673@gmail.com>
2025-11-07 08:29:25 +0000

    Python Programming

--- Day9II.py
+++ Day9II.py
@@ -17,11 +17,16 @@ x = {
 
 json_converter = json.dumps(x)
 
+contacts = []
+
+contacts.append(x)
+
 folder_path = r"C:\Users\user\Desktop\beginner\json"
 file_path = os.path.join(folder_path,f"{name}_contact.json")
 
 with open(file_path, "w") as file:
     json.dump(x,file,indent=4)
     
+
         
 
