5f58b3aee38d1a1bba7dce2c1f1ad2c772cc8dcb
Michael Sintim <michaelsintim673@gmail.com>
2025-11-07 09:55:05 +0000

    Python Programming

---
+++ Day10.py
@@ -0,0 +1,2 @@
+#Day 10
+## Build a program that converts between JSON and CSV formats with  serialization
--- Day9.py
+++ Day9.py
@@ -1,4 +1,5 @@
 #Create a program that asks users for their favorite books and saves them to a text file
+import os
 
 name = input("What is your name:")
 book = 0
@@ -11,8 +12,12 @@ while True:
         if book == 5:
             break
         
+    
+folder_path = r"C:\Users\user\Desktop\beginner\txt_files"
 
-with open(rf"C:\Users\user\Desktop\beginner\contacts{name}_books.txt", 'w') as file:
+file_path = os.path.join(folder_path,f"{name}_book.txt")
+
+with open(file_path, 'w') as file:
     for i, book in enumerate(book_list,start=1):
          file.write(f"Book {i}: {book}\n")
 
